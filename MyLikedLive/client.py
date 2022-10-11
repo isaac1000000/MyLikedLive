646ea00a8454fd9ee359db08b40feb9d118ce793
isaac1000000 <isaac.fisher@nyu.edu>
2022-10-11 18:42:05 -0400

    Gather useful information

--- MyLikedLive/client.py
+++ MyLikedLive/client.py
@@ -4,12 +4,18 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+# This scope allows for reading of recently listened
 scope = "user-read-recently-played"
 redirect_uri = "https://google.com"
 
+# Creates a spotipy instance with the determined scope
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_recently_played()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " - ", track['name'])
+# Creates a set of the unique artists in the user's recently played
+query_results = sp.current_user_recently_played()
+unique_artists = set()
+for item in query_results['items']:
+    for artist in item['track']['artists']:
+        unique_artists.add(artist['name'])
+
+print(unique_artists)
