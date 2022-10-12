29390b89c6107815ee30c905e15824c8c4fa447c
isaac1000000 <isaac.fisher@nyu.edu>
2022-10-12 15:07:26 -0400

    implemented searching by proximity and artist
    new methods __get_relevant_concerts in concert_locator
    
    dmaIDs.csv used as reference for user

--- MyLikedLive/concert_locator.py
+++ MyLikedLive/concert_locator.py
@@ -3,27 +3,43 @@
 
 import requests
 import os
+import json
 
 class ConcertLocator:
     # Checks location for current on every instance, stored in external .txt file
     # for user continuity
     with open("loc.txt", 'r') as l:
         location = l.read()
+    tm_api_key = os.getenv("TICKETMASTER_API_KEY")
+
+    # Gets the artist key for the artist to use in __get_relevant_concerts()
+    def __get_artist_key(self):
+        artist_search_url = ("https://app.ticketmaster.com/discovery/v2/attractions.json?"
+        "keyword={artist}&"
+        "apikey={tmapi}").format(artist=self.artist, tmapi=self.tm_api_key)
+        data = requests.get(artist_search_url).json()
+        self.artist_key = data["_embedded"]["attractions"][0]["id"]
 
 #TODO: Error handling on request
 #TODO: Filter out irrelevant (far) concerts
 #TODO: Find best practice for returning and working with results
-    def __getRelevantConcerts(self):
-        data = requests.get(self.url).json()
+#TODO: call __get_artist_key() and use results to filter by artist
+    # Searches concerts for artist in the user's location
+    def __get_relevant_concerts(self):
+        self.__get_artist_key()
+        concert_search_url = ("https://app.ticketmaster.com/discovery/v2/events.json?"
+        "classificationName=music&" # Narrows to concerts
+        "dmaId=324&" # Location
+        "attractionId={artist_key}&" # Location
+        "apikey={tmapi}").format(artist_key=self.artist_key, tmapi=self.tm_api_key) # authorizes
+        data = requests.get(concert_search_url).json()
+        print(data["_embedded"]["events"][0])
 
-
+#TODO: Ping server for artist's name and get their dmaID
+#TODO: Currently searches by location, url construction needs to be changed
     # Basic constructor, creates URL for later use in webscraping
     def __init__(self, artist):
         self.artist = artist
-        self.url = ("https://app.ticketmaster.com/discovery/v2/events.json?"
-        "classificationName=music&" # Narrows to concerts
-        "dmaId=324&" # Artist
-        "apikey={tmapi}").format(tmapi=os.getenv("TICKETMASTER_API_KEY")) # authorizes
-        self.__getRelevantConcerts()
+        self.relevant_concerts = self.__get_relevant_concerts()
 
 cl1 =  ConcertLocator("Men I Trust")
--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 Hello!
-This is a bit of a side project to play around with some webscraping techniques.
+This is a bit of a side project to play around with gathering API data.
 If you find it useful, would like to help out, or want to contact me for any reason, you can reach me at isaac.fisher@nyu.edu
 Feel free to take this framework and improve it! Just let me know so I can see what cool things people are doing.
 Cheers🥂
@@ -7,8 +7,12 @@ Cheers🥂
 Working list of used libraries:
 spotipy
 
-Procedure for authorizing this program:
+Procedure for using this program:
 Set local variables using these commands:
      export SPOTIPY_CLIENT_ID=(your spotipy client id)
      export SPOTIPY_CLIENT_SECRET=(your spotipy client secret)
-These will set environment variables in your shell and allow you to run the program without explicitly typing your keys
+     export TICKETMASTER_API_KEY=(your spotipy api key)
+Choose your local market:
+     In resources/dmaIDs.csv, find an appropriate local market
+     Change the value in MyLikedLive/loc.txt to the number listed (default NYC)
+These will set environment variables in your shell and allow you to run the program without hardcoding your keys
