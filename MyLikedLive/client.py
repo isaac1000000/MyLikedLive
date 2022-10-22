607804a8d377fb788cd6ba68cb62fd1150181f6c
isaac1000000 <isaac.fisher@nyu.edu>
2022-10-22 11:16:26 -0400

    delete references, reinstate login method

--- MyLikedLive/client.py
+++ MyLikedLive/client.py
@@ -4,10 +4,10 @@ from PyQt6.QtCore import QSize
 import os.path
 
 class MainWindow(QMainWindow):
-#    def login(self):
-#        if os.path.exists(".cache"):
-#            os.remove(".cache")
-#        self.ss = SpotifyScraper()
+    def login(self):
+        if os.path.exists(".cache"):
+            os.remove(".cache")
+        self.ss = SpotifyScraper()
 
 
     def make_login_window(self, stack):
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow):
         stack.instruction_label.setText("Press the button to connect to spotify")
 
         stack.connect_button = QPushButton("Connect to spotify")
-#        stack.connect_button.clicked.connect(self.login)
+        stack.connect_button.clicked.connect(self.login)
 
         layout = QVBoxLayout()
         layout.addWidget(stack.instruction_label)
@@ -64,12 +64,6 @@ ex = MainWindow()
 ex.show()
 app.exec()
 
-
-#app = QApplication([])
-#login_window = LoginWindow()
-#login_window.show()
-#app.exec()
-
 #print()
 #print("Lately, you've been listening to: ")
 #print(", ".join(ss.unique_artists))
@@ -82,63 +76,3 @@ app.exec()
 #for concert in ss.all_concerts:
 #    print(concert)
 #print()
-
-
-class stackedExample(QWidget):
-
-   def __init__(self):
-      super(stackedExample, self).__init__()
-      self.leftlist = QListWidget()
-      self.leftlist.insertItem (0, 'Contact' )
-      self.leftlist.insertItem (1, 'Personal' )
-      self.leftlist.insertItem (2, 'Educational' )
-
-      self.stack1 = QWidget()
-      self.stack2 = QWidget()
-      self.stack3 = QWidget()
-
-      self.stack1UI()
-      self.stack2UI()
-      self.stack3UI()
-
-      self.Stack = QStackedWidget(self)
-      self.Stack.addWidget(self.stack1)
-      self.Stack.addWidget(self.stack2)
-      self.Stack.addWidget(self.stack3)
-
-      hbox = QHBoxLayout(self)
-      hbox.addWidget(self.leftlist)
-      hbox.addWidget(self.Stack)
-
-      self.setLayout(hbox)
-      self.leftlist.currentRowChanged.connect(self.display)
-      self.setGeometry(300, 50, 10,10)
-      self.setWindowTitle('StackedWidget demo')
-      self.show()
-
-   def stack1UI(self):
-      layout = QFormLayout()
-      layout.addRow("Name",QLineEdit())
-      layout.addRow("Address",QLineEdit())
-      #self.setTabText(0,"Contact Details")
-      self.stack1.setLayout(layout)
-
-   def stack2UI(self):
-      layout = QFormLayout()
-      sex = QHBoxLayout()
-      sex.addWidget(QRadioButton("Male"))
-      sex.addWidget(QRadioButton("Female"))
-      layout.addRow(QLabel("Sex"),sex)
-      layout.addRow("Date of Birth",QLineEdit())
-
-      self.stack2.setLayout(layout)
-
-   def stack3UI(self):
-      layout = QHBoxLayout()
-      layout.addWidget(QLabel("subjects"))
-      layout.addWidget(QCheckBox("Physics"))
-      layout.addWidget(QCheckBox("Maths"))
-      self.stack3.setLayout(layout)
-
-   def display(self,i):
-      self.Stack.setCurrentIndex(i)
